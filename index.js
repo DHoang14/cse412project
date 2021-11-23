0b0a6501c813c2c9120b15b9f7ed4725521c22ac
DHoang14 <dhoang9@asu.edu>
2021-11-23 11:21:51 -0700

    implemented somewhat working search bar

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'pug')
 const PORT = process.env.PORT || 3000;
 
 const {Client} = require('pg')
@@ -16,8 +17,12 @@ client.connect();
 let output;
 const setOutput = (rows) => {
 	output = rows;
+	
 }
 
+var carrier = [];
+
+//example of a query and passing in an array to index.pug
 let query = 'Select carriername, carrierid from Carrier';
 client.query (query, (err, rows) => {
 	if (err) {
@@ -25,12 +30,53 @@ client.query (query, (err, rows) => {
 		return;
 	}
 	setOutput(rows);
+	
+	
+	for (var i = 0; i < output.rows.length; i++) {
+		carrier.push(output.rows[i].carriername);
+	}
+});
+
+//home tab
+app.get('/home', (req, res) => {
+	res.render('index.pug', { name: output.rows[0].carriername, id: output.rows[0].carrierid, carrier: carrier, buttonclick: buttonclick()} );
+})
+
+//search results
+app.get('/search', function(req, res, next) {
+	var searchstring = req.query.searchBar;
+	query = "Select carrierName FROM Carrier WHERE carrierName = '" + searchstring + "'";
+	var results =[]
+	
+	client.query (query, (err, rows) => {
+		if (err) {
+			console.log("error");
+			return;
+		}
+		
+		for (var i = 0; i <rows.rows.length; i++) {
+			results.push(rows.rows[i].carriername);
+		}
+		
+		res.render('results.pug', {result: results});
+	})
+	
 });
 
 
+//example of passing in a function to index.pug
+const buttonclick =  () =>{
+	console.log('clicked');
+	console.log();
+	return 1;
+}
+
+//initial home page
 app.use('/', (req, res, next) => {
-	res.render('index.pug', { name: output.rows[0].carriername, id: output.rows[0].carrierid});
+	res.render('index.pug', { name: output.rows[0].carriername, id: output.rows[0].carrierid, carrier: carrier, buttonclick: buttonclick()} );
 });
+
+
 
 app.listen(PORT, () => {
 	console.log('app listening on PORT');
